cfb401530dc98e03a8c9e517f099013d0af068ec
Mariusz Kamiński <mariuszkam4@wp.pl>
2023-10-31 13:32:03 +0100

    Uwtorzono widoki dla modeli

--- reservations/views.py
+++ reservations/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Hairdresser, Service, Reservation
 
-# Create your views here.
+def hairdresser_list(request):
+    haidressers = Hairdresser.objects.all()
+    return render(request, 'hairdresser_list.html', {'hairdressers': haidressers})
+
+def service_list(request):
+    services = Service.objects.all()
+    return render(request, 'service_list.html', {'services': services})
+
+def reservation_list(request):
+    reservations = Reservation.objects.all()
+    return render(request, 'reservations_list.html', {'reservations': reservations})
