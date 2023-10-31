6d5fc36209a6b6aec7095065550e527b99f54482
Mariusz Kamiński <mariuszkam4@wp.pl>
2023-10-31 13:31:20 +0100

    Zarejestrowano modele w panelu admin

--- reservations/admin.py
+++ reservations/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Hairdresser, Service, Reservation
 
-# Register your models here.
+admin.site.register(Hairdresser)
+admin.site.register(Service)
+admin.site.register(Reservation)
