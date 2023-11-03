5c992685bd87b5d95bb2f07360158430d20bf0f5
Mariusz Kamiński <mariuszkam4@wp.pl>
2023-11-03 15:01:50 +0100

    Dodano test sprawdzający dodwanie fryzjerów i ich specjalności

--- reservations/tests.py
+++ reservations/tests.py
@@ -1,3 +1,41 @@
 from django.test import TestCase
+from .models import Hairdresser, SpecializationChoice
 
-# Create your tests here.
+class HairdresserSpecializationTestCase(TestCase):
+    def setUp(self):
+        # Tworzenie specjalizacji
+        SpecializationChoice.objects.create(specialization='M')
+        SpecializationChoice.objects.create(specialization='F')
+        SpecializationChoice.objects.create(specialization='S')
+        
+        # Tworzenie fryzjerów
+        self.hairdresser1 = Hairdresser.objects.create(name='Fryzjer1')
+        self.hairdresser2 = Hairdresser.objects.create(name='Fryzjer2')
+        self.hairdresser3 = Hairdresser.objects.create(name='Fryzjer3')
+
+        # Przypisywanie specjalizacji
+        self.hairdresser1.specialization.add(
+            SpecializationChoice.objects.get(specialization='M')
+        )
+        self.hairdresser2.specialization.add(
+            SpecializationChoice.objects.get(specialization='F')
+        )
+        # Fryzjer 3 ma dwie specjalizacje
+        self.hairdresser3.specialization.add(
+            SpecializationChoice.objects.get(specialization='F'),
+            SpecializationChoice.objects.get(specialization='S')
+        )
+
+    def test_specialization_assignment(self):
+        # Testowanie przypisania specjalizacji dla fryzjera 1
+        self.assertEqual(self.hairdresser1.specialization.count(), 1)
+        self.assertTrue(self.hairdresser1.specialization.filter(specialization='M').exists())
+
+        # Testowanie przypisania specjalizacji dla fryzjera 2
+        self.assertEqual(self.hairdresser2.specialization.count(), 1)
+        self.assertTrue(self.hairdresser2.specialization.filter(specialization='F').exists())
+
+        # Testowanie przypisania specjalizacji dla fryzjera 3
+        self.assertEqual(self.hairdresser3.specialization.count(), 2)
+        self.assertTrue(self.hairdresser3.specialization.filter(specialization='F').exists())
+        self.assertTrue(self.hairdresser3.specialization.filter(specialization='S').exists())
