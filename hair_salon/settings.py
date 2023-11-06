d6f2d9290cf18fc73c2ed352a173c06deb8220d6
Mariusz Kamiński <mariuszkam4@wp.pl>
2023-11-06 18:13:14 +0100

    Dodano Django Query Logging w celu śledzenia interakcji z bazą

--- hair_salon/settings.py
+++ hair_salon/settings.py
@@ -27,6 +27,29 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+LOGGING = {
+    'version': 1,
+    'filters': {
+        'require_debug_true': {
+            '()': 'django.utils.log.RequireDebugTrue',
+        }
+    },
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'filters': ['require_debug_true'],
+            'class': 'logging.StreamHandler',
+        }
+    },
+    'loggers': {
+        'django.db.backends': {
+            'level': 'DEBUG',
+            'handlers': ['console'],
+        }
+    }
+}
+
+
 ALLOWED_HOSTS = []
 
 # Application definition
