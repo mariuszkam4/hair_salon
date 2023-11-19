8ff09bbba8aaf227353fda7b62dcd4c16318f007
Mariusz Kamiński <mariuszkam4@wp.pl>
2023-11-19 22:54:57 +0100

    Poprawiono bug w zakresie braku pola end_time w formularzu rejestracji rezerwacji - pole jest już dodane

--- reservations/forms.py
+++ reservations/forms.py
@@ -13,32 +13,46 @@ logger = logging.getLogger(__name__)
 class ReservationForm(forms.ModelForm):
     class Meta:
         model = Reservation
-        fields = ['hairdresser', 'service', 'start_date', 'start_time']
+        fields = ['hairdresser', 'service', 'start_date', 'start_time', 'end_time']
         widgets = {
             'start_date': forms.DateInput(attrs={'type': 'date'}),
             'start_time': forms.TimeInput(attrs={'type': 'time'}),
+            'end_time': forms.TimeInput(attrs={'type': 'time', 'required': False}),
         }
 
     def clean(self):
         cleaned_data = super().clean()
         start_date = cleaned_data.get('start_date')
         start_time = cleaned_data.get('start_time')
+        end_time = cleaned_data.get('end_time')
         service = cleaned_data.get('service')
 
         if start_date and start_time and service:
             start_datetime = timezone.make_aware(datetime.combine(start_date, start_time))
-            end_datetime = start_datetime + service.duration
-            cleaned_data['end_time'] = end_datetime.time()
+            if end_time:
+                end_datetime = timezone.make_aware(datetime.combine(start_date, end_time))
+                if end_datetime < start_datetime:
+                    raise ValidationError(_('Czas zakończenia nie może być wcześniejszy niż czas rozpoczęcia.'))
+            else:
+                # Obliczenie domyślnego czasu zakończenia na podstawie czasu trwania usługi
+                end_datetime = start_datetime + service.duration
+                cleaned_data['end_time'] = end_datetime.time()
+
         else:
             raise ValidationError(_('Data i godzina rozpoczęcia oraz usługa są wymagane.'))
 
         return cleaned_data
-    
+
     def save(self, commit=True):
         instance = super(ReservationForm, self).save(commit=False)
-        if not instance.end_time and instance.service and instance.start_time:
-            # Ustaw czas zakończenia na podstawie czasu trwania usługi
-            instance.end_time = instance.start_time + instance.service.duration
+        if instance.start_date and instance.start_time and instance.service:
+            start_datetime = timezone.make_aware(datetime.combine(instance.start_date, instance.start_time))
+            if instance.end_time:
+                end_datetime = timezone.make_aware(datetime.combine(instance.start_date, instance.end_time))
+                instance.end_time = end_datetime.time()
+            else:
+                instance.end_time = (start_datetime + instance.service.duration).time()
+
         if commit:
             instance.save()
         return instance
