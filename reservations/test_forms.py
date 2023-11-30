028a8335c6863cfacdd4eb001b27c8f6adf49ac8
Mariusz Kamiński <mariuszkam4@wp.pl>
2023-11-30 13:37:22 +0100

    Uzupełniono testy formularza ReservationForm o testowanie metody save

--- reservations/test_forms.py
+++ reservations/test_forms.py
@@ -1,17 +1,20 @@
 from django.test import TestCase
 from .forms import ReservationForm
-from .models import Hairdresser, Service, SpecializationChoice
-from datetime import timedelta
+from .models import Hairdresser, Service, SpecializationChoice, Reservation
+from datetime import timedelta, date, time
 
 class ReservationFormTests(TestCase):
 
     def setUp(self):
+        self.specialization = SpecializationChoice.objects.create(specialization="M")
         self.hairdresser = Hairdresser.objects.create(name="Testowy Fryzjer")
+        self.hairdresser.specialization.add(self.specialization)
         self.service = Service.objects.create(
             name="Strzyżenie", 
             duration = timedelta(hours=1), 
             cost=100
-        )
+        )        
+        self.service.specializations.add(self.specialization)
     
     def test_valid_data(self):
         form = ReservationForm(data={
@@ -21,6 +24,7 @@ class ReservationFormTests(TestCase):
             "start_time": "10:00",
             'end_time': '11:00'
         })
+        print (form.errors)
         self.assertTrue(form.is_valid())
 
     def test_invalid_data(self):
@@ -37,7 +41,8 @@ class ReservationFormTests(TestCase):
             'hairdresser': self.hairdresser.id,
             'service': self.service.id,
             'start_date': '2023-01-01',
-            'start_time': '09:00'
+            'start_time': '10:00',
+            'end_time': '09:00'
         })
         self.assertFalse(form.is_valid())
         
@@ -49,7 +54,60 @@ class ReservationFormTests(TestCase):
             'start_date': '2023-01-01',
             'start_time': '09:00'
         })
+        print (form.errors)
         self.assertTrue(form.is_valid())
         self.assertIsNotNone(form.cleaned_data['end_time'])
 
+    def test_save_new_reservation(self):
+        # Sprawdzenie czy metoda save poprawnie tworzy nową instancję modelu Reservation
+        form = ReservationForm(data={
+            'hairdresser': self.hairdresser.id,
+            'service': self.service.id,
+            'start_date': '2023-01-01',
+            'start_time': '10:00',
+            'end_time': "11:00"
+        })
+        if form.is_valid():
+            reservation = form.save()
+            self.assertIsNotNone(reservation.pk)
+            self.assertEqual(reservation.hairdresser, self.hairdresser)
+            self.assertEqual(reservation.service, self.service)
+        else:
+            self.fail("Formularz powinien być poprawny")
     
+    def test_update_existing_reservation(self):
+        reservation = Reservation.objects.create(
+            hairdresser = self.hairdresser,
+            service = self.service,
+            start_date=date(2023, 1, 1),
+            start_time=time(10, 0),
+            end_time=time(11, 0)
+        )
+
+        form = ReservationForm(instance=reservation, data={
+             'hairdresser': self.hairdresser.id,
+            'service': self.service.id,
+            'start_date': '2023-01-02',
+            'start_time': '11:00',
+            'end_time': "12:00"
+        })
+        if form.is_valid():
+            updated_reservation = form.save()
+            self.assertEqual(updated_reservation.start_date, date(2023, 1, 2))
+            self.assertEqual(updated_reservation.start_time, time(11, 0))
+            self.assertEqual(updated_reservation.end_time, time(12, 0))
+        else:
+            self.fail("Formularz powinien być poprawny")
+    
+    def test_save_with_auto_end_time(self):
+        form = ReservationForm(data={
+             'hairdresser': self.hairdresser.id,
+            'service': self.service.id,
+            'start_date': '2023-01-02',
+            'start_time': '11:00',
+        })
+        if form.is_valid():
+            reservation = form.save()
+            self.assertIsNotNone(reservation.end_time)
+        else:
+            self.fail("Formularz powinien być poprawny")
